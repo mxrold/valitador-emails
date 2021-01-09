1f498afadf4fcc5d0bc27a56e9fecfd9a4f377cd
Martin Roldan <martinroldan889@gmail.com>
2021-01-09 16:23:14 -0300

    Validación simple del email con JS

--- js/app.js
+++ js/app.js
@@ -30,14 +30,20 @@ function validarFormulario(e) {
         console.log('Si hay algo')
     } else {
         e.target.classList.add('border', 'border-red-500');
+        mostrarError('Todos los campos son obligatorios');
+    }
 
-        mostrarError();
+    if(e.target.type === 'email') {
+        const resultado = e.target.value.indexOf('@');
+        if(resultado < 0) {
+            mostrarError('El email no es válido');
+        }
     }
 }
 
-function mostrarError() {
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('border', 'border-red-500','background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error');
